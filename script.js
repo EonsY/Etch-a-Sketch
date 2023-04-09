16ad9ed41c3d339958d27838502a19059d4d48e2
EonsY <leicheewah54@gmail.com>
2023-04-09 14:00:55 +0800

    Project finished, requirements achieved

--- script.js
+++ script.js
@@ -1,34 +1,88 @@
-// Grid creation
+// Grid Changing
 const grid = document.getElementById("grid-container");
+let divs = Array.from(document.querySelectorAll('.grid-item'));
 
-function gridCreate() {
+divCreate = function() {
     let gridItem = document.createElement("div");
     gridItem.classList.add('grid-item');
     grid.appendChild(gridItem);
 }
 
-for (let i = 0; i < 16**2; i++) {
-    gridCreate();
+function divRefresh() {
+    divs = Array.from(document.querySelectorAll('.grid-item'));
+}
+
+function gridClear() {
+    divRefresh();
+    divs.forEach(div => div.remove());
+}
+
+function gridCreate(num) {
+    gridClear();
+    grid.style.setProperty('grid-template-columns', `repeat(${num}, auto)`);
+    for (let i = 0; i < num**2; i++) {
+        divCreate();
+    };
+    divs.forEach(div => div.addEventListener('click', pen));
 };
 
-// Colouring
-function colouring(e) {
-    if (e.target.classList[1] == 'coloured') {
-        e.target.classList.remove('coloured');
-    } else {
-        e.target.classList.add('coloured');
+let gridChangeVal = false;
+function gridChangeFunc() {
+    if (!gridChangeVal) {
+        gridCreate(64);
+        gridChangeVal = true;
+    } else if (gridChangeVal) {
+        gridCreate(16);
+        gridChangeVal = false;
+    }
+}
+
+gridCreate(16);
+
+// Pen
+grid.onmousedown = function() {
+    divRefresh();
+    divs.forEach(div => div.addEventListener('mouseover', pen));
+}
+grid.onmouseup = function() {
+    divRefresh();
+    divs.forEach(div => div.removeEventListener('mouseover', pen));
+}
+
+let colorVal = false
+function coloring() {
+    colorVal = true;
+}
+
+function erasing() {
+    colorVal = false;
+}
+
+function pen(e) {
+    if (colorVal) {
+    e.target.classList.add('coloured');
+    } else if (!colorVal) {
+    e.target.classList.remove('coloured');
     };
 }
 
-function clearAll() {
+// Clear
+function clear() {
+    divRefresh();
     for (let i = 0; i < divs.length; i++) {
         if (divs[i].classList.contains('coloured')) {
             divs[i].classList.remove('coloured');
         }
-    }
+    };
 }
 
-const divs = Array.from(document.querySelectorAll('.grid-item'));
-const button = document.querySelector('.clear-all');
-divs.forEach(div => div.addEventListener('click', colouring));
-button.addEventListener('click', clearAll);
+// Eventlisteners
+const colorMode = document.querySelector('#color-mode');
+const eraserMode = document.querySelector('#eraser-mode');
+const gridChange = document.querySelector('#grid-change');
+const clearAll = document.querySelector('#clear-all');
+
+colorMode.addEventListener('click', coloring);
+eraserMode.addEventListener('click', erasing);
+gridChange.addEventListener('click', gridChangeFunc);
+clearAll.addEventListener('click', clear);
